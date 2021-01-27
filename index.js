920212067b3d9d7f10395bb1496684f5f6989deb
shajjadKabir <sajjadkabir71@gmail.com>
2021-01-27 19:12:11 +0600

    dice js done

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6 
+ 
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1 -dice6 .png 
+
+var randomImageSource = "images/" + randomDiceImage; // images/dice1 -dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//for player 2 dice
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+//player wins part 
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "player 1 wins";
+}
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "player 2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
